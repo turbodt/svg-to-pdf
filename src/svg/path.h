5e1739c475ad9f1e4f5557efabf8657af40701a6
Daniel T <daniel.torres.moral@upc.edu>
2025-07-07 15:24:33 +0200

    WIP: Added SVG Path API and string parser

---
+++ src/svg/path.h
@@ -0,0 +1,32 @@
+#ifndef APP_SRC_SVG_PATH_H
+#define APP_SRC_SVG_PATH_H
+
+
+#include "../geometry.h"
+
+
+typedef enum {
+    SVG_PATH_CMD_UNKNOWN,
+    SVG_PATH_CMD_LINE,
+    SVG_PATH_CMD_QBEZIER,
+    SVG_PATH_CMD_CBEZIER
+} SVGPathCommandType;
+
+
+typedef struct {
+    SVGPathCommandType type;
+    Point2D points[4];
+} SVGPathCommand;
+
+
+typedef struct SVGPath SVGPath;
+
+
+SVGPath *svg_path_make_from_string(char const *s);
+void svg_path_destroy(SVGPath *path);
+int svg_path_command_count(SVGPath const *path);
+SVGPathCommand const *svg_path_get_command(SVGPath const *path, unsigned int index);
+void svg_path_apply_transform(SVGPath *path, double matrix[6]);
+
+
+#endif
